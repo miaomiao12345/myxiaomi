583e96240e8fb5e919b34047cd8e5a14e3a10dd0
miaomiao12345 <2579014375@qq.com>
2019-06-19 23:13:31 +0800

    git 上传不忽略build

---
+++ build/precache-manifest.872053e3667e5929dba6d3227051c618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7b71fcc46cebb90fa1c2",
+    "url": "./static/js/main.194dc7cc.chunk.js"
+  },
+  {
+    "revision": "be9ee05be395c82ea200",
+    "url": "./static/js/2.81b74b66.chunk.js"
+  },
+  {
+    "revision": "7b71fcc46cebb90fa1c2",
+    "url": "./static/css/main.25a9c976.chunk.css"
+  },
+  {
+    "revision": "ad45a89c9d4b63f0f0c9d952c5afbbe1",
+    "url": "./index.html"
+  }
+];
